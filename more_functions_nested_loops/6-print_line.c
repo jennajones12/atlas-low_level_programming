997573506badf83530638365589ef7857fb90a6e
Jenna Jones <jljones121@gmail.com>
2024-01-27 14:04:10 +0000

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - draw straight line in terminal
+ * @n: length of line
+ * Return: zero
+ */
+
+void print_line(int n) {
+    if (n <= 0) {
+        _putchar('\n');
+    } else {
+        for (int i = 0; i < n; i++) {
+            _putchar('_');
+        }
+        _putchar('\n');
+    }
+}
