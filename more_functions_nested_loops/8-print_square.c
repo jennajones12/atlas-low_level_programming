bc69a23c9b34b7af0d0964ec937e40e8ad7ff874
Jenna Jones <jljones121@gmail.com>
2024-01-27 14:37:37 +0000

    betty fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,19 @@
  */
 void print_square(int size)
 {
-    int i, j;
-    if (size > 0)
-    {
-        for (i = 0; i < size; i++)
-        {
-            for (j = 0; j < size; j++)
-            {
-                putchar('#');
-            }
-            putchar('\n');
-        }
-    }
-    else
-    {
-        putchar('\n');
-    }
-}
-
+       	int i, j;
+       	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+		       	for (j = 0; j < size; j++)
+			{
+				putchar('#');
+			}
+			putchar('/n');
+		}
+		else
+		{
+			putchar('/n');
+		}
+	}
