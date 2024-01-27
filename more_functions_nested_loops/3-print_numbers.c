58fe36e1fb6c03ca7bbd7e9330c003d6010766d4
Jenna Jones <jljones121@gmail.com>
2024-01-27 02:59:26 +0000

    fixed code to prevent value changes

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  * followed by a new line
  * Return: 0
  */
-void print_numbers (void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= '9'; i++)
-		_putchar (i + '0');
-	_putchar ('/n');
+	for (i = 0; i <= 9; i++)
+	_putchar (i + '0');
+_putchar ('\n');
 }
