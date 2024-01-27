b55a1f25f7c8f366bc614a8ec570f356fb774758
Jenna Jones <jljones121@gmail.com>
2024-01-27 14:31:50 +0000

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: size of square
+ * Return: zero
+ */
+void print_square(int size)
+{
+    int i, j;
+    if (size > 0)
+    {
+        for (i = 0; i < size; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                putchar('#');
+            }
+            putchar('\n');
+        }
+    }
+    else
+    {
+        putchar('\n');
+    }
+}
+
