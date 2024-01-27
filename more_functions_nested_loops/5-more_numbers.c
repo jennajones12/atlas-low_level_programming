4ccd6c55a86c685f92f324ab88ba0f7da980b4e9
Jenna Jones <jljones121@gmail.com>
2024-01-27 13:48:33 +0000

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14, a new line
+ *
+ * Return: null
+ */
+
+void more_numbers(void) {
+    for (int i = 0; i < 15; i++) {
+        printf("%d\n", i * 10);
+    }
+}
