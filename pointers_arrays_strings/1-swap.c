b8cca31fffa7ea68e7373dc3256c36e995bdddfc
Jenna Jones <jljones121@gmail.com>
2024-01-30 04:56:46 +0000

    swap code

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap values
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+void swap_int(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
