6e2d8f1806928710343d868ad55f8f64d4f36884
Jenna Jones <jljones121@gmail.com>
2024-01-30 06:42:03 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,12 +6,16 @@
  * Return: pointer destination
  */
 
-char *_strcpy(char *dest, char *src) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+    while (src[i] != '\0')
+
+    {
+	    dest[i] = src [i];
+	i++;
     }
-    dest[i] = '\0';
-    return dest;
+    dest[i] = '\0'
+    return (dest);
 }
