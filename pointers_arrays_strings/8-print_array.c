88494b3136b3ca0ceb938ce8ae0bd83733df3873
Jenna Jones <jljones121@gmail.com>
2024-01-30 06:03:48 +0000

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,13 +4,10 @@
  * @a: interger for array
  * @n: elements in the array
  */
-void print_array(int *a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
+void print_array(int *a, int n) {
+    for (int i = 0; i < n; i++) {
         printf("%d", a[i]);
-        if (i < n - 1) 
-	{
+        if (i < n - 1) {
             printf(", ");
         }
     }
