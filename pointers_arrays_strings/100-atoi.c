cbd936b3b535a94b9523a360f0325eba8a6f68dc
Jenna Jones <jljones121@gmail.com>
2024-01-30 07:38:44 +0000

    new attempt

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,25 +10,33 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
-while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-{
-	i++;
-}
-
-if (s[i] == '-' || s[i] == '+')
-{
 	if (s[i] == '-')
 	{
 		sign = -1;
+		i++;
+}
+
+	if (s[i] == '+')
+	{
+		i++;
 	}
-	i++;
-}
 
-while (s[i] >= '0' && s[i] <= '9')
-{
-	result = result * 10 + (s[i] - '0');
-	i++;
-}
+	while (s[i] != '\0')
+	{
 
-return sign * result;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+
+		}
+		i++;
+	}
+
+	result = result * sign;
+
+	return result;
 }
