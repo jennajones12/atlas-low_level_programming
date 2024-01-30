e9f2f84c6890cc26848b0885cc5dccc93f916b06
Jenna Jones <jljones121@gmail.com>
2024-01-30 07:17:16 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - function that converts string to integer
+ * @s: input string
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+{
+	i++;
+}
+
+if (s[i] == '-' || s[i] == '+')
+{
+	if (s[i] == '-')
+	{
+		sign = -1;
+	}
+	i++;
+}
+
+while (s[i] >= '0' && s[i] <= '9')
+{
+	reult = result * 10 + (s[i] - '0');
+	i++;
+}
+
+return sign * result;
+}
