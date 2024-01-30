fcc3a03446c5ea183d93b14b02e4b42abc8bce2a
Jenna Jones <jljones121@gmail.com>
2024-01-30 05:12:00 +0000

    strlen compile error

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
  * _strlen - returns length of string
  * @s: string length parameter
  * Return: string length value
  *
  */
-int_strlen(char *s)
-{
-	int length = 0;
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return length;
+int _strlen(char *s) {
+    int length = 0;
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+    return length;
 }
