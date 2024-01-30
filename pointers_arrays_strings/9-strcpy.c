b9ac458e3bd7fd7bdfeed53325a11025b20bc567
Jenna Jones <jljones121@gmail.com>
2024-01-30 06:29:19 +0000

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - cp pointed string, inc \0, to buffer dest
+ * @dest: pointed to string
+ * @src: given string
+ * Return: pointer destination
+ */
+char *_strcpy(char *dest, char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
