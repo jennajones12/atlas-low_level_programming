492b9ffb44a37b767ab2c770cc2818a5c2be36f5
Jenna Jones <jljones121@gmail.com>
2024-01-30 07:45:39 +0000

    new attempt

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,39 +4,22 @@
  * @s: input string
  * Return: an integer
  */
-int _atoi(char *s)
-{
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+int custom_atoi(char *s) {
+    int result = 0;
+    int i = 0;
+    int sign = 1;
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-}
+    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)) {
+        i++;
+    }
 
-	if (s[i] == '+')
-	{
-		i++;
-	}
+    if (s[i] == '-' || s[i] == '+') {
+        sign = (s[i++] == '-') ? -1 : 1;
+    }
 
-	while (s[i] != '\0')
-	{
+    while (s[i] >= '0' && s[i] <= '9') {
+        result = result * 10 + (s[i++] - '0');
+    }
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-
-		}
-		i++;
-	}
-
-	result = result * sign;
-
-	return result;
+    return sign * result;
 }
