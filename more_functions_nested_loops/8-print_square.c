d75d2ca66d01fe8bc7b27b73bccd3e2dca2293d4
Jenna Jones <jljones121@gmail.com>
2024-01-30 03:59:17 +0000

    compiling

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,20 @@
  */
 void print_square(int size)
 {
-	if (size <= 0) {
-		_putchar('\n');
-	} else {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+    int i, j;
+    if (size > 0)
+    {
+        for (i = 0; i < size; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                putchar('#');
+            }
+            putchar('\n');
+        }
+    }
+    else
+    {
+        putchar('\n');
+    }
 }
