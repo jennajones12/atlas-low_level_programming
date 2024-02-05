24246c9001e640a445eaf744041b78e01c616961
Jenna Jones <jljones121@gmail.com>
2024-02-05 16:05:02 +0000

    capstring

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,22 @@ char *cap_string(char *str)
 {
 int i;
 
-  for (i = 0; str[i] != '\0'; i++)
-  {
-    if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-        str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-        str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-        str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-        str[i - 1] == '}')
-    {
-      if (str[i] >= 'a' && str[i] <= 'z')
-      {
-        str[i] = str[i] - 32;
-      }
-    }
-  }
+for (i = 0; str[i] != '\0'; i++)
 
-  return (str);
+{
+	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+	str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+	str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+	str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+	str[i - 1] == '}')
+{
+	if (str[i] >= 'a' && str[i] <= 'z')
+
+{
+	str[i] = str[i] - 32;
+}
+}
+}
+
+return (str);
 }
