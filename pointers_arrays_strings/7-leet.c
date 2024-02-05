1fce29f17ba724f19ebf4c4cff4a49dcd742c496
Jenna Jones <jljones121@gmail.com>
2024-02-05 13:33:10 +0000

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,24 +4,39 @@
  * @str: input string
  * Return: String that is encoded
  */
-char *leet(char *str) 
+char* leet(char* str) 
 {
-int i;
+char *result = str;
 
-  for (i = 0; str[i] != '\0'; i++)
-  {
-	if (str[i] == 'a') str[i] = '4';
-        else if (str[i] == 'e') str[i] = '3';
-        else if (str[i] == 'o') str[i] = '0';
-        else if (str[i] == 't') str[i] = '7';
-        else if (str[i] == 'l') str[i] = '1';
+if (str == NULL) 
+{
+return NULL;
+}
 
-        else if (str[i] == 'A') str[i] = '4';
-        else if (str[i] == 'E') str[i] = '3';
-        else if (str[i] == 'O') str[i] = '0';
-        else if (str[i] == 'T') str[i] = '7';
-        else if (str[i] == 'L') str[i] = '1';
-    }
-    
-    return str;
+while (*str) 
+{
+        if (*str == 'a' || *str == 'A')
+{
+            *str = '4';
+} 
+else if (*str == 'e' || *str == 'E') 
+{
+            *str = '3';
+} 
+else if (*str == 'o' || *str == 'O') 
+{
+            *str = '0';
+} 
+else if (*str == 't' || *str == 'T') 
+{
+            *str = '7';
+} 
+else if (*str == 'l' || *str == 'L') 
+{
+            *str = '1';
+}
+        str++;
+}
+
+    return (result);
 }
