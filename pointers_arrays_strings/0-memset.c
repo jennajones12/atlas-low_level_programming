c71cd308f894f97043bb709abafe855178104de8
Jenna Jones <jljones121@gmail.com>
2024-02-05 16:20:52 +0000

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: first input string
+ * @b: second input string
+ * @n:unsigned int
+ * Return: returns pointer in area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{	
+		s[i] = b;
+	}
+
+	return (s);
+}
