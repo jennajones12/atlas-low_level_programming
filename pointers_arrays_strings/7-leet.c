0eb4409bc912f91d76cf3888a8d16be98875a307
Jenna Jones <jljones121@gmail.com>
2024-02-05 15:54:28 +0000

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,27 +6,25 @@
  */
 char *leet(char *n)
 {
-	char leetMap[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char leetReplace[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+char leetMap[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char leetReplace[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	int i = 0;
+int i = 0;
 
-	while (n[i] != '\0')
+while (n[i] != '\0')
+{
+	int j = 0;
 
+	while (j < 10)
 	{
-		int j = 0;
-
-		while (j < 10)
-
+		if (n[i] == leetMap[j])
 		{
-			if (n[i] == leetMap[j])
-			{
 			n[i] = leetReplace[j];
-	break;
-			}
-			j++;
+			break;
 		}
-		i++;
+		j++;
 	}
-	return (n);
+	i++;
+}
+return (n);
 }
