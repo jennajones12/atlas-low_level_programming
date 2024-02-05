e426a0f2e42b58a0db4c9808b02518135f87ffc7
Jenna Jones <jljones121@gmail.com>
2024-02-05 14:24:34 +0000

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,39 +4,25 @@
  * @str: input string
  * Return: String that is encoded
  */
-char* leet(char* str) 
+char *leet(char *str) 
 {
-char *result = str;
+    int i = 0;
+    while (str[i] != '\0') {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            if (str[i] == 'a' || str[i] == 'A') {
+                str[i] = '4';
+            } else if (str[i] == 'e' || str[i] == 'E') {
+                str[i] = '3';
+            } else if (str[i] == 'o' || str[i] == 'O') {
+                str[i] = '0';
+            } else if (str[i] == 't' || str[i] == 'T') {
+                str[i] = '7';
+            } else if (str[i] == 'l' || str[i] == 'L') {
+                str[i] = '1';
+            }
+        }
+        i++;
+    }
 
-if (str == NULL) 
-{
-return NULL;
-}
-
-while (*str) 
-{
-        if (*str == 'a' || *str == 'A')
-{
-            *str = '4';
-} 
-else if (*str == 'e' || *str == 'E') 
-{
-            *str = '3';
-} 
-else if (*str == 'o' || *str == 'O') 
-{
-            *str = '0';
-} 
-else if (*str == 't' || *str == 'T') 
-{
-            *str = '7';
-} 
-else if (*str == 'l' || *str == 'L') 
-{
-            *str = '1';
-}
-        str++;
-}
-
-    return (result);
+    return str;
 }
