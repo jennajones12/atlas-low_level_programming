e385de3d82c9a993f5396d20646dd1b3f21e7ba9
Jenna Jones <jljones121@gmail.com>
2024-02-05 09:46:16 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-
-    dest[i] = '\0';
-    return (dest);
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
