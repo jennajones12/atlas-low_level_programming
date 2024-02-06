33c0930e48decb41e6b7e331a179ad726149aacd
Jenna Jones <jljones121@gmail.com>
2024-02-06 07:10:03 +0000

    'chessboard'

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,32 +1,23 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdio.h>
-
-/**
- * print_row - prints a row of the chessboard
- * @row: pointer to the row of the chessboard
- *
- * Description: Recursively prints a row of the chessboard.
- */
-void print_row(char *row) {
-    if (*row == '\0') {
-        putchar('\n');
-        return;
-    }
-    putchar(*row);
-    putchar(' ');
-    print_row(row + 1);
-}
-
 /**
  * print_chessboard - prints the chessboard
  * @a: 2D array representing the chessboard
- *
- * Description: Recursively prints the entire chessboard.
  */
-void print_chessboard(char (*a)[8]) {
-    if (*a == NULL)
-        return;
-    print_row(*a);
-    print_chessboard(a + 1);
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+{
+	j = 0;
+	while (j < 8)
+{
+	putchar(a[i][j]);
+	j++;
+}
+putchar('\n');
+i++;
+}
 }
