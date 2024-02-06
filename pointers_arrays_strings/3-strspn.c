3324cadd5945e4623584d20a377ec0fe5f32a29a
Jenna Jones <jljones121@gmail.com>
2024-02-06 04:40:33 +0000

    'strspn'

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: characters to search for
+ *
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	while (*s != '\0' &&
+strchr(accept, *s) != NULL)
+{
+count++;
+s++;
+}
+return (count);
+}
