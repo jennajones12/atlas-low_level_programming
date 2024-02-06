e688167260a9ed8e0195b7c11a3ca3a39a680da4
Jenna Jones <jljones121@gmail.com>
2024-02-06 08:25:24 +0000

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * print_diagsums - calculates and  prints primary, secondary sums
@@ -12,36 +10,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sum_diag1 = a[0] + a[size + 1] + a[size * 2];
-	int sum_diag2 = a[size - 1] + a[size + 1] + a[size * 2 - 2];
+int primary_sum = 0;
+int secondary_sum = 0;
 
-	printf("Sum of primary diagonal: %d\n", sum_diag1);
-	printf("Sum of secondary diagonal: %d\n", sum_diag2);
+int i = 0;
+while (i < size)
 {
-	int matrix[3][3];
+	primary_sum += a[i * size + i];
+	secondary_sum += a[i * size + (size - 1 - i)];
+	i++;
 }
 
-	srand(time(NULL));
-	
-	matrix[0][0] = rand() % 100 + 1;
-	matrix[0][1] = rand() % 100 + 1;
-	matrix[0][2] = rand() % 100 + 1;
-	matrix[1][0] = rand() % 100 + 1;
-	matrix[1][1] = rand() % 100 + 1;
-	matrix[1][2] = rand() % 100 + 1;
-	matrix[2][0] = rand() % 100 + 1;
-	matrix[2][1] = rand() % 100 + 1;
-	matrix[2][2] = rand() % 100 + 1;
-{
-
-	printf("Matrix:\n");
-	printf("%d\t%d\t%d\n", matrix[0][0], matrix[0][1], matrix[0][2]);
-	printf("%d\t%d\t%d\n", matrix[1][0], matrix[1][1], matrix[1][2]);
-	printf("%d\t%d\t%d\n", matrix[2][0], matrix[2][1], matrix[2][2]);
-
-	printf("\nDiagonal Sums:\n");
-	print_diagsums((int *)matrix, 3);
-
-	return (0);
+printf("Sum of primary diagonal: %d\n", primary_sum);
+printf("Sum of secondary diagonal: %d\n", secondary_sum);
 }
-}
+
