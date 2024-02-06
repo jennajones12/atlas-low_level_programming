ca51ab77c6171b2ddcabb244bcfaa2f70117022b
Jenna Jones <jljones121@gmail.com>
2024-02-06 05:59:54 +0000

    'chess'

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,31 +2,25 @@
 /**
  * print_chessboard - prints the chessboard
  * @a: 2D array representing the chessboard
+ * 'a' represents the black squares and ' ' represents the white squares.
  *
  */
 void print_chessboard(char (*a)[8])
 {
     int i = 0, j = 0;
 
+    FILE *stream = stdout;
+
     while (i < 8)
     {
         j = 0;
         while (j < 8)
         {
-            a[i][j] |= 1UL << 7;
+            fputc(a[i][j], stream);
+            fputc(' ', stream);
             j++;
         }
-        i++;
-    }
-    i = 0;
-    while (i < 8)
-    {
-        j = 0;
-        while (j < 8)
-        {
-            a[i][j] &= ~(1UL << 7);
-            j++;
-        }
+        fputc('\n', stream);
         i++;
     }
 }
