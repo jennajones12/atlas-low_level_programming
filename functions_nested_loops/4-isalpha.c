677e2b7c980ed6591557297265ac7f619b508641
Jenna Jones <jljones121@gmail.com>
2024-02-07 05:39:09 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if input c is an alphabet
+ * @c: character for the check
+ * Return: if c is a letter, return 1; otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if(isalpha(c))
+{
+return (1);
+}
+else
+{
+	return (0);
+}
+}
