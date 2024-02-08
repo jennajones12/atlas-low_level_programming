731676757bb957628327d5059d3086b79e688e69
Jenna Jones <jljones121@gmail.com>
2024-02-08 02:17:19 +0000

    reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to print string in reverse, 
+ * followed by new line.
+ * @s: an input string to printing
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+
+		putchar(*s);
+
+	}
+}
