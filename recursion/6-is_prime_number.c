ef5019e488bbfb2a044f2a5ace07bb89cf62a25e
Jenna Jones <jljones121@gmail.com>
2024-02-11 12:17:15 +0000

    _added_sqrt_helper

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * _sqrt - helper for _sqrt_recursion
+ * @n: input number
+ * @x: iterator
+ *
+ * Return: 1 if @n is prime, 0 otherwise
+ */
+int _sqrt(int n, int x)
+{
+	if (x == 1)
+	{
+		return (1);
+	}
+	if (n % x == 0)
+	{
+		return (0);
+	}
+	return (_sqrt(n, x - 1));
+}
 
 /**
  * is_prime_number - checks if number is prime number
@@ -9,24 +27,9 @@
  */
 int is_prime_number(int n)
 {
-
 	if (n <= 1)
-		return (0);
-
-	if (n == 2)
-		return (1);
-
-	if (n % 2 == 0)
-		return (0);
-
-	int r = 3;
-
-	while (r * r <= n)
 	{
-		if (n % r == 0)
-			return (0);
-		r+= 2;
+	return (0);
 	}
-	
-	return (1);
+	return (_sqrt(n, n / 2) ? 1 : 0);
 }
