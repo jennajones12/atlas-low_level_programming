79d4af42fdcd78bb31af244fec295a39cd4bb2f8
Jenna Jones <jljones121@gmail.com>
2024-02-11 11:37:43 +0000

    removed_mixed_declaration

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,14 +12,19 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 
-int divisor =2;
+	if (n == 2)
+		return (1);
 
-while (divisor * divisor <= n)
-{
-	if (n % divisior == 0)
+	if (n % 2 == 0)
 		return (0);
-	divisor++;
-}
 
-return (1);
+	int i = 3;
+	while (i * i <= n)
+	{
+		if (n % i == 0)
+			return (0);
+		i+= 2;
+	}
+	
+	return (1);
 }
