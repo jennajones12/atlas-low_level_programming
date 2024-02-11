bf8185822d008533849df6f08c2d5e23a2acd51c
Jenna Jones <jljones121@gmail.com>
2024-02-11 13:15:03 +0000

    pushing

--- static_libraries/libmy.c
+++ static_libraries/libmy.c
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#include "main.h"
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,5 +20,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
