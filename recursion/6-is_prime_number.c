6de35656b9b7fd6217bef8e8de198e96c151a13d
Jenna Jones <jljones121@gmail.com>
2024-02-11 11:24:20 +0000

    _no_nest_fnction

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,28 +7,19 @@
  *
  * Return: 1 if @n is prime, 0 otherwise
  */
-
-/**
- * is_prime_helper - Recursive helper function to check primality.
- * @n: The input integer to check.
- * @divisor: The current divisor to test.
- *
- * Return: 1 if prime, 0 otherwise.
- */
-
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
 
-int is_prime_helper(int n, int divisor)
-{
-	if (divisor * divisor > n)
-		return (1);
-	
-	if (n % divisor == 0)
-		return (0);
+int divisor =2;
 
-	return (is_prime_helper(n, divisor + 1));
+while (divisor * divisor <= n)
+{
+	if (n % divisior == 0)
+		return (0);
+	divisor++;
 }
+
+return (1);
 }
