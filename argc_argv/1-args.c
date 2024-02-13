a481149dea65adc7e5946456367b09b6f543be38
Jenna Jones <jljones121@gmail.com>
2024-02-13 02:23:24 +0000

    main-return-value_betty-doc-error

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,11 +5,13 @@
  * main - prints number of arguments passed into it
  * @argc: number of arugments passed
  * @argv: value of arguments passed
- * return: always 0
+ * return: always 0, on success
  */
 int main(int argc, char *argv[])
 {
+	int main(void)
 	if (argv[0])
 		printf("%d\n", argc - 1);
+
 	return (0);
 }
