e7a725bfd73f26dcae69386e4548d2ca22c1f29a
Jenna Jones <jljones121@gmail.com>
2024-02-13 05:12:38 +0000

    3mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - prints all arguments received
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 (Success), 1 if Error
+ */
+
+int atoi(const char *str);
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
