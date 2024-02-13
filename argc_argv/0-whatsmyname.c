da8559734f79e30282669ad2cde34ffd543dbfe5
Jenna Jones <jljones121@gmail.com>
2024-02-13 01:50:48 +0000

    push_files

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints its name, followed by new line
+ * @argc: argument count
+ * @argv: argument value
+ * Return: always 0
+ */
+int main (int argc, char *argv[])
+{
+	printf("Program name: %s\n", argv[0]);
+	return (0);
+}
