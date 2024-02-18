1ea50abce1a4cf976865e9c3d2ed1b6119e69064
Jenna Jones <jljones121@gmail.com>
2024-02-18 23:39:59 +0000

    commit-need-to-allocate-mem

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,4 +7,36 @@
  *
  *
  */
+char *_strdup(char *str)
+{
+	char *new_str, *start;	
+	int i = 0, len = 0;
 
+	if (str == NULL)
+        	return (NULL);
+
+	start = str;
+	
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	str = start;
+	new_str = malloc(sizeof(char) * (len + 1));
+	start = new_str;
+
+	if (new_str == NULL)
+	{
+
+    for (i = 0; i < len; i++) 
+    {
+        new_str[i] = str[i];
+    }
+    new_str[len] = '\0';
+    return (start);
+	}
+	else
+		return (NULL);
+}
