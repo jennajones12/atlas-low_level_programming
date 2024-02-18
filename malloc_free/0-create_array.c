274f7ff2c9eeed892b8ea2e66749a3f249aa3177
Jenna Jones <jljones121@gmail.com>
2024-02-18 22:59:40 +0000

    no-memset

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /*
  *
@@ -11,5 +10,21 @@
 
 char *create_array(unsigned int size, char c)
 {
-	return (size > 0) ? (char *)memset(malloc(size), c, size) : (NULL);
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+	
+	if (s == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }
