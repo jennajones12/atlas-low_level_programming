8089f354af9de859ac83b89c84b50e448ff64f3b
Jenna Jones <jljones121@gmail.com>
2024-02-19 02:40:28 +0000

    betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,14 @@
 #include <limits.h>
 
 /**
- *
- *
- *
+ * malloc_checked - function that allocates memory using malloc
+ * @b: unsigned input integer
+ * return: pointer to allocated memory or NULL if it fails
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
+	
 	if (ptr == NULL)
 	{
 		exit(98);
