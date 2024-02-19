7458719e8dd664f5396bc02b690a4d51ba95e4c3
Jenna Jones <jljones121@gmail.com>
2024-02-19 01:20:39 +0000

    betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * free_grid - frees mem allocated for 2D array of integers.
+ * @grid: pointer to the 2D array of integers.
+ * @height: height of the 2D array.
  *
  */
 void free_grid(int **grid, int height)
