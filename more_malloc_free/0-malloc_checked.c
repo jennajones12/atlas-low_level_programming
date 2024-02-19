d43e874f47c858aac98b6b3723ce63889d7394f8
Jenna Jones <jljones121@gmail.com>
2024-02-19 02:36:46 +0000

    malloc-checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ *
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
