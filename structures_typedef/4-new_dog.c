12498ff12d0896d83aa259e8d6f2a620b14ab340
Jenna Jones <jljones121@gmail.com>
2024-02-26 13:33:01 +0000

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,35 +11,53 @@
 int string_length(const char *str)
 {
 	int len = 0;
-	while (str[len} != '\0')
+	while (str[len] != '\0')
 {
 	len++;
 }
 return (len);
 }
 
-void 
+void string_copy(char *dest, const char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	int nameLen = string_length(name);
+	int ownerLen = string_length(owner);
+
+	new_dog->name = (char *)malloc(nameLen + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->age = age;
-
-	new_dog->owner = strdup(owner);
+	string_copy(new_dog->name, name);
+	
+	new_dog->owner = (char *)malloc(ownerLen + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+	string_copy(newDog->owner, owner);
+
+	new_dog->age = age;
+
 	return (new_dog);
 }
