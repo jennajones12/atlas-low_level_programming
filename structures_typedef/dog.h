00ea3eeb6fbf3e73b791c9671ed13952d3e4f2ea
Jenna Jones <jljones121@gmail.com>
2024-02-26 07:03:21 +0000

    1-poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef _DOG_H
 #define _DOG_H
 #include <stdio.h>
-
 /**
  * struct dog - structure that defines new type with givin elements
  * @name: dog's name
@@ -9,19 +8,15 @@
  * @owner: dog's owner
  * Return: none
  */
-
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char owner;
-} 
-dog_t;
-
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *check_null(char *s);
 #endif
-
