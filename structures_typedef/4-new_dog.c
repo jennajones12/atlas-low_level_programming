f73335c24e4cc8d4c20d3dcc29e5fb42778cc9b4
Jenna Jones <jljones121@gmail.com>
2024-02-26 18:40:55 +0000

    omg-everything-isnt-free-now

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,7 +15,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[len_name])
 		len_name++;
 	while (owner[len_owner])
@@ -26,15 +25,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
+	if (cpyname == NULL) {
+		free(new_dog);
 		return (NULL);
+	}
 	for (i = 0; name[i]; i++)
 		cpyname[i] = name[i];
 	cpyname[i] = '\0';
 
 	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
+	if (cpyowner == NULL) {
+		free(cpyname);
+		free(new_dog);
 		return (NULL);
+	}
 	for (i = 0; owner[i]; i++)
 		cpyowner[i] = owner[i];
 	cpyowner[i] = '\0';
