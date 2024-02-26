f68f5bf7fa2314a07ee39388931b5382f26336e3
Jenna Jones <jljones121@gmail.com>
2024-02-26 17:43:34 +0000

    stillworkingoncodetocompile

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
  * new_dog - creates new dog structure
  * @name: dog's name
@@ -12,32 +12,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *cpyname, *cpyowner;
-	int name_len = 0, owner_len = 0;
+	int len_name = 0, len_owner = 0, i;
 
 	if (name == NULL || owner == NULL)
 		return NULL;
 
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	
-	new_dog->name = (cpyname); malloc((name_len + 1) * sizeof(char));
-	if (new_dog->name == NULL)
-	 {
-		 free(new_dog);
+	cpyname = malloc((len_name + 1);
+	if (cpyname == NULL)
 		 return (NULL);
-	 }
-	 new_dog->owner = (cpyowner); malloc((owner_len + 1) * sizeof(char));
-	 if (new_dog->owner == NULL)
-	 {
-		 free(new_dog);
-		 return (NULL);
-	 }
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+	cpyname[i] = '\0';
+
+	 cpyowner = malloc(len_owner + 1);
+	 if (cpyowner == NULL)
+	 	return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+	cpyowner[i] = '\0';
+
 	 new_dog->name = cpyname;
 	 new_dog->age = age;
 	 new_dog->owner = cpyowner;
