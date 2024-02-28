cc5dbab61a8fb5442a34bdc9e4feedf6501323c3
Jenna Jones <jljones121@gmail.com>
2024-02-28 06:05:03 +0000

    printnumbs

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,17 +10,25 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	for (i = 0; i < n; i++)
+	va_list ap;
+	unsigned int i = 0;
+	char *string;
+
+	va_start(ap, n);
+	for (; i < n; i++)
+
 	{
-		str = va_arg(args, char*);
-		printf("%s", str ? str : "(nil)");
-		if (i < n - 1 && separator != NULL)
-		{
+
+		string = va_arg(ap, char*);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+		if (separator == NULL)
+			continue;
+		if (i < n - 1)
 			printf("%s", separator);
-		}
 	}
-
 	printf("\n");
-	va_end(args);
+	va_end(ap);
 }
-
