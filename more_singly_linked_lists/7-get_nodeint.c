3bfc824a56123c56a1a124176b9cdb938c8f8ab6
Jenna Jones <jljones121@gmail.com>
2024-03-11 07:57:31 +0000

    kernel

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,10 @@
 #include <stddef.h>
 
 /**
- *
+ * get_nodeint_at_index - Returns the nth node of a listint_t linked list
+ * @head: A pointer to the head of the listint_t list
+ * @index: The index of the node to return, starting at 0
+ * Return: A pointer to the node at the specified index, or NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
