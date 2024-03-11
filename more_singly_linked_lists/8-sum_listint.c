5a493b58f9eb3415729730a13f88b61497d09d6e
Jenna Jones <jljones121@gmail.com>
2024-03-11 08:02:48 +0000

    betty

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,9 @@
 #include <stddef.h>
 
 /**
- *
+ * sum_listint - Sums all the data (n) of a listint_t linked list
+ * @head: A pointer to the head of the listint_t list
+ * Return: The sum of all the data (n) in the list
  */
 
 int sum_listint(listint_t *head)
