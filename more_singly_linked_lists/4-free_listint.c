d20ed3bbe9483f5f1a163eadd87c3dfbd5ceea7f
Jenna Jones <jljones121@gmail.com>
2024-03-11 07:37:42 +0000

    betty

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * free_listint - Frees a listint_t list
+ * @head: A pointer to the head of the listint_t list to be freed
  */
 
 void free_listint(listint_t *head)
