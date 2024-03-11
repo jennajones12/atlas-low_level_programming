d4d26ccdffbd5d973bee724073ee580ff2553253
Jenna Jones <jljones121@gmail.com>
2024-03-11 08:13:42 +0000

    kernel

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
-	new_node = (listint_t *)malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
