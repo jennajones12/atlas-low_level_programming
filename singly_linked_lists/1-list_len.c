e11c1722f62040db8f50c2442b1f54a88ef662f5
Jenna Jones <jljones121@gmail.com>
2024-03-11 05:40:40 +0000

    list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - lists number of nodes in linked list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	 const list_t *current = h;
+	 size_t count = 0;
+
+	 while (current != NULL)
+	 {
+		 count++;
+		 current = current->next;
+	 }
+
+	 return (count);
+}
