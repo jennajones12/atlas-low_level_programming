8a42430e4c24c68a5a5083a282a523501f3d3d0c
Jenna Jones <jljones121@gmail.com>
2024-03-11 07:18:50 +0000

    betty

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: A pointer to a pointer to the head of the listint_t list
+ * @n: The integer to be added as the new node's n member
+ * Return: The address of the new element, or NULL if it failed
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp;
