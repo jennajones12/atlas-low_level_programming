c054cba7de23b39efd9e9cb953e54f8f41aaaf0d
Jenna Jones <jljones121@gmail.com>
2024-03-11 08:11:40 +0000

    kernel

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 
 /**
- *
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: A pointer to a pointer to the head of the listint_t list
+ * @idx: The index of the list where the new node should be added. Index starts at 0
+ * @n: The integer to be inserted into the new node
+ * Return: The address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
