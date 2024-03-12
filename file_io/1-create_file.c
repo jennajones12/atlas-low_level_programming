60da6b4a9ae3deca5244ae48c5b515143ab059c8
Jenna Jones <jljones121@gmail.com>
2024-03-12 03:28:51 +0000

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,10 @@
 #include "main.h"
 
 /**
- *
+ * create_file - Creates a file with specified content
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
