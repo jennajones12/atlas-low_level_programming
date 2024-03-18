8796d1aeec8570daf3369298d2fbc985e2642d36
Jenna Jones <jljones121@gmail.com>
2024-03-18 23:50:28 +0000

    edits on c:15, 19, 26; version now adheres to kernel style.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,18 +12,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	 dlistint_t *new_node;
 
-	 if (head == NULL)
+	 if (!head)
 		 return (NULL);
 
 	 new_node = malloc(sizeof(dlistint_t));
-	 if (new_node == NULL)
+	 if (!new_node)
 		 return (NULL);
 
 	 new_node->n = n;
 	 new_node->prev = NULL;
 	 new_node->next = *head;
 
-	 if (*head != NULL)
+	 if (*head)
 		 (*head)->prev = new_node;
 
 	 *head = new_node;
