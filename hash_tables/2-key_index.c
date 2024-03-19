452d8a02ef4f4239b30d5493bf237197d02f7868
Jenna Jones <jljones121@gmail.com>
2024-03-19 04:39:03 +0000

    Edits- c:8, 14; removed spaces for Betty.

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,13 +5,13 @@
 
 /**
  * key_index - Returns the index at which the key/value pair should be stored
- * 		in the array of the hash table
+ * in the array of the hash table
  * @key: The key
  * @size: The size of the array of the hash table
  * Return: The index at which the key/value pair should be stored
  */
 
- unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_val;
 
