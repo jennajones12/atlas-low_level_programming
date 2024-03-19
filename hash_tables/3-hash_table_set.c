5b54252fd6c0eb26b6963d63a5a55ef5b677a064
Jenna Jones <jljones121@gmail.com>
2024-03-19 05:19:22 +0000

    Edits for Betty broke my original compile-- more edits for fix. >:[

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,10 +32,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp->value = strdup(value);
 
 			if (temp->value == NULL)
-			{
 				return (0);
-				return (1);
-			}
+			return (1);
 		}
 		temp = temp->next;
 	}
