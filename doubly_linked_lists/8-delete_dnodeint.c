b2cea606b184cde305472b74d333769cfac4422e
Jenna Jones <jljones121@gmail.com>
2024-03-19 00:43:32 +0000

    Edits- c:6, less than 80 chars now. c:17, 18, 20, 21, 23, 26, 27, 29 -- tabstops

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes node at index of dlistint_t linked list
  * @head: double pointer to the head of the list
  * @index: index of the node to delete
  *  Return: 1 if succeeded, -1 if failed
@@ -14,30 +14,30 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	 dlistint_t *current = *head;
 	 unsigned int i;
 
-	 if (*head == NULL)
-		 return (-1);
+	if (*head == NULL)
+		return (-1);
 
-	 if (index == 0)
-	 {
+	if (index == 0)
+	{
 		 *head = current->next;
-		 if (*head != NULL)
-			 (*head)->prev = NULL;
-		 free(current);
-		 return (1);
-	 }
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-	 for (i = 0; current != NULL && i < index; i++)
+	for (i = 0; current != NULL && i < index; i++)
 		 current = current->next;
 
-	 if (current == NULL)
-		 return (-1);
+	if (current == NULL)
+		return (-1);
 
-	 if (current->next != NULL)
-		 current->next->prev = current->prev;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
-	 if (current->prev != NULL)
-		 current->prev->next = current->next;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 
-	 free(current);
-	 return (1);
+	free(current);
+	return (1);
 }
