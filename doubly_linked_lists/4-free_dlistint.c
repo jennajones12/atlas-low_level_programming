27b65814a576ac8717347d661cdd235c9f6afaac
Jenna Jones <jljones121@gmail.com>
2024-03-19 00:09:27 +0000

    Edit tabstops on c:13, 14, 18; kernel style code.

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -10,10 +10,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
 
-	 while (head != NULL)
-	 {
+	while (head != NULL)
+	{
 		 current = head;
 		 head = head->next;
 		 free(current);
-	 }
+	}
 }
