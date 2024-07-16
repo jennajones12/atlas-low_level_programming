a0510a6438ed48759da8ad8cd900bb124a5cbc1c
Jenna Jones <jljones121@gmail.com>
2024-07-16 11:37:07 +0000

    3,4 kernel. 5.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,14 +19,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (*head == NULL) {
+	if (*head == NULL) 
+	{
 		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 
 	last = *head;
-	while (last->next != NULL) 
+	while (last->next != NULL)
 		last = last->next;
 
 	last->next = new_node;
--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -11,7 +11,7 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
-	while (head != NULL) 
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
