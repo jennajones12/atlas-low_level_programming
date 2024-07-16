fd1c5743d623818f977e469f97f6be9aafa44c12
Jenna Jones <jljones121@gmail.com>
2024-07-16 11:53:34 +0000

    7kernel

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,33 +16,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp;
 	unsigned int i;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
+	if (idx == 0) return (add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	 new_node->n = n;
+	new_node->n = n;
 
-	 temp = *h;
-	 for (i = 0; temp != NULL && i < idx; i++)
-	 {
-		 if (i == idx - 1)
-		 {
-			 if (temp->next == NULL)
-				 return (add_dnodeint_end(h, n));
+	temp = *h;
+	for (i = 0; temp != NULL && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
 
-			 new_node->next = temp->next;
-			 new_node->prev = temp;
-			 if (temp->next != NULL)
-				 temp->next->prev = new_node;
-			 temp->next = new_node;
-			 return (new_node);
-		 }
-		 temp = temp->next;
-	 }
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
+			temp->next = new_node;
+			return (new_node);
+		}
+		temp = temp->next;
+	}
 
-	 free(new_node);
-	 return (NULL);
+	free(new_node);
+	return (NULL);
 }
