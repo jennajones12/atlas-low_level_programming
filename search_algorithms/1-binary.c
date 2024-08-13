aba2f5f8e14edb14be31de123712f177221ab69e
Jenna Jones <jljones121@gmail.com>
2024-08-13 00:09:36 -0500

    1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,44 +1,57 @@
 #include "search_algos.h"
 
-int binary_find(int *array, size_t size, int value)
+/**
+ * print_array - Prints the elements of an array between two indices
+ *
+ * @array: Pointer to the first element of the array to search in
+ * @left: Starting index of the array
+ * @right: Ending index of the array
+ */
+void print_array(int *array, size_t left, size_t right)
 {
-    return (binary_find_recursive(array, 0, size - 1, value));
-}
-
-int binary_find_recursive(int *array, size_t left, size_t right, int value)
-{
-    size_t mid;
-
-    if (!array)
-        return (-1);
-
-    display_array(array, left, right);
-    mid = left + (right - left) / 2;
-
-    if (array[mid] == value)
-        return ((int)mid);
-
-    if (left < right)
-    {
-        if (array[mid] < value)
-            return (binary_find_recursive(array, mid + 1, right, value));
-        else
-            return (binary_find_recursive(array, left, mid - 1, value));
-    }
-
-    return (-1);
-}
-
-void display_array(int *array, size_t left, size_t right)
-{
-    size_t idx;
+    size_t i;
 
     printf("Searching in array: ");
-    for (idx = left; idx <= right; idx++)
+    for (i = left; i <= right; i++)
     {
-        printf("%d", array[idx]);
-        if (idx < right)
+        printf("%d", array[i]);
+        if (i < right)
             printf(", ");
     }
     printf("\n");
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array of integers
+ *                 using the Binary search algorithm
+ *
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The index where value is located, or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t mid;
+
+    if (array == NULL)
+        return (-1);
+
+    while (left <= right)
+    {
+        print_array(array, left, right);
+        mid = left + (right - left) / 2;
+
+        if (array[mid] == value)
+            return (mid);
+        else if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return (-1);
 }
