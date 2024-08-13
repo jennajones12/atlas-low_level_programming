3e97dfc3828e5a1b628ac294931008dea9bce74e
Jenna Jones <jljones121@gmail.com>
2024-08-13 00:07:47 -0500

    implentation file update and header file change

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int linear_find(int *array, size_t size, int value);
-int binary_find(int *array, size_t size, int value);
-int binary_find_recursive(int *array, size_t left, size_t right, int value);
-void display_array(int *array, size_t left, size_t right);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t left, size_t right, int value);
+void print_array(int *array, size_t left, size_t right);
 
 #endif /* _SEARCH_ALGOS_H_ */
